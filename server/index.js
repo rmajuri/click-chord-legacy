d0665b928e39c685685d3c1db76eed317823e4d8
Rob Majuri <robmajuri@gmail.com>
2019-01-21 17:16:40 -0500

    deploy

--- server/index.js
+++ server/index.js
@@ -49,9 +49,7 @@ const createApp = () => {
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
-  )
+  app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
 }
 
 async function bootApp() {
